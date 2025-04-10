b69405071253ed8df1769c76ab124c32818cbcec
Omar Ashraf <omr.ashrf.prog@gmail.com>
2025-04-10 15:48:57 +0200

    v0.1.0 -> fixed the calculation method and More..

--- src/app.py
+++ src/app.py
@@ -1,8 +1,9 @@
 from os import chdir, path
-import geocoder
+from geocoder import ip
 import requests
 import json
 from datetime import datetime, timedelta
+from threading import Thread
 
 # Change cwd to -> fwd
 chdir(path.dirname(path.abspath(__file__)))
@@ -14,7 +15,7 @@ tomorrow = datetime.now() + timedelta(days=1)
 
 
 def get_prayer_times():
-    ip_location = geocoder.ip('me')
+    ip_location = ip('me')
     lat, lng = ip_location.latlng
     # جلب أوقات الصلاة باستخدام الإحداثيات
     url = f"https://api.aladhan.com/v1/calendar?latitude={lat}&longitude={lng}&month={month}&year={year}&method=4"
@@ -25,55 +26,63 @@ def get_prayer_times():
     print(f"Saved as prayer_data({month}-{year}).json")   
 
 
-def find_sunset_sunrise(today, tomorrow):
-    with open(f"data/prayer_data({month}-{year}).json", "r", encoding="utf-8") as json_file:
-       prayer_data = json.load(json_file)
+def fetch_night_times(prayer_data, start_time="Isha", end_time="Fajr"):
      
     for day in prayer_data["data"]:
         if day['date']['readable'] == today:
-            today_fajr = day['timings']['Fajr'].strip("(EET)")
-            today_sunset = day['timings']['Sunset'].strip("(EET)")
+            today_night_end_time = day['timings'][end_time].strip("(EET)")
+            night_start_time = day['timings'][start_time].strip("(EET)")
         elif day['date']['readable'] == tomorrow:      
-            tomorrow_fajr = day['timings']['Sunrise'].strip("(EET)")
+            tomorrow_night_end_time = day['timings'][end_time].strip("(EET)")
              
     try:
-        return (today_sunset, tomorrow_fajr)
+        return (night_start_time.strip(), tomorrow_night_end_time.strip())
     except:
-        return (today_sunset, today_fajr)
+        return (night_start_time.strip(), today_night_end_time.strip())
     
     
-def qyam_equaiton(today_sunset, tomorrow_fajr):
-    sunset_dt = datetime.strptime(today_sunset, "%H:%M")
-    fajr_dt = datetime.strptime(tomorrow_fajr, "%H:%M") + timedelta(days=1)  # إضافة يوم للشروق
+def qyam_equaiton(start_night, end_night):
+    start_night_dt = datetime.strptime(start_night, "%H:%M")
+    end_night_dt = datetime.strptime(end_night, "%H:%M") + timedelta(days=1)  # إضافة يوم للشروق
 
-    duration = fajr_dt - sunset_dt
+    duration = end_night_dt - start_night_dt
     sixth = duration / 6
-    midnight = sunset_dt + duration / 2
-    fourth_and_fifth_sixth = (sunset_dt + (sixth * 4)).time()
-    six_sixth = (sunset_dt + (sixth * 5)).time()
+    midnight = start_night_dt + duration / 2
+    fourth_and_fifth_sixth = (start_night_dt + (sixth * 4)).time()
+    six_sixth = (start_night_dt + (sixth * 5)).time()
     
-    print(f"All night: {duration}")
-    print(f"One sixth: {sixth}")
-    print(f"Midnight: {midnight.time()}")
-    print(f"4 and 5 sixth starts at: {fourth_and_fifth_sixth}, ends at:{six_sixth}")
-    print(f"the 6 sixth starts at: {six_sixth}")
+    #print(f"All night: {duration}")
+    #print(f"One sixth: {sixth}")
+    #print(f"Midnight: {midnight.time()}")
+    #print(f"4 and 5 sixth starts at: {fourth_and_fifth_sixth}, ends at:{six_sixth}")
+    #print(f"the 6 sixth starts at: {six_sixth}")
     
-    #fourth_and_fifth_sixth = (sunset_dt + sixth * 3, sunset_dt + sixth * 5)
-    #return fourth_and_fifth_sixth
+    calculation =  {
+        "allnight": str(duration),
+        "midnight": str(midnight.time()),
+        "fourth_and_fifth_sixth": str(fourth_and_fifth_sixth),
+        "six_sixth": str(six_sixth)        
+    }
     
-
-
+    print(calculation)
+    return calculation
+    
+    
 def main():
     if path.exists(f"data/prayer_data({month}-{year}).json"):
-        print(f"Exists -> prayer_data({month}-{year}).json")
-        today_sunset, tomorrow_fajr = find_sunset_sunrise(today, tomorrow)
-        qyam_equaiton(today_sunset.strip(), tomorrow_fajr.strip())
+        #print(f"Exists -> prayer_data({month}-{year}).json")
+        with open(f"data/prayer_data({month}-{year}).json", "r", encoding="utf-8") as json_file:
+            prayer_data = json.load(json_file)
+            qyam_equaiton(*fetch_night_times(prayer_data))
         
 
     else:
-        print(f"Not Exists -> prayer_data({month}-{year}).json")
+        #print(f"Not Exists -> prayer_data({month}-{year}).json")
         get_prayer_times()
-        qyam_equaiton(today_sunset.strip(), tomorrow_fajr.strip())
+        with open(f"data/prayer_data({month}-{year}).json", "r", encoding="utf-8") as json_file:
+            prayer_data = json.load(json_file)
+            
+            qyam_equaiton(today_sunset.strip(), tomorrow_fajr.strip())
         
     
 main()    
--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
 import flet as ft
 
+def main(page: ft.Page) -> None:
+    ft.title = "simple-chat"
+    
 
+if __name__ == "__main__":
+    ft.app(target=main, assets_dir="assets")
