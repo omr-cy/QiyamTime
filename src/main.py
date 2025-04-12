b62eb465f2103d97471faed62fe3ef89f37e1511
omrAshrf <omr.ashrf.porg.com>
2025-04-12 15:01:57 +0200

    big update to main.py and widgets folder

--- src/main.py
+++ src/main.py
@@ -1,8 +1,24 @@
 import flet as ft
+from widgets import my_appbar
 
 def main(page: ft.Page) -> None:
-    ft.title = "simple-chat"
+    page.title = "قيام الأنبياء"
+    page.vertical_alignment = "center"
+    page.horizontal_alignment = "center"
+    
+    
+    # Setup Controls | تهيئة عناصر التطبيق
+    page.appbar = my_appbar()
     
 
+
+
+
+
+
+
+
+
+    page.update()
 if __name__ == "__main__":
     ft.app(target=main, assets_dir="assets")
---
+++ src/widgets/__init__.py
@@ -0,0 +1,3 @@
+from .appbar import my_appbar
+#from .navbar import my_nav_bar
+#from .fab import my_fab
